3ebfdc49a3b624bdb9a659f96740c2a1c90caecd
Khalid <kar.khalid@gmail.com>
2018-07-07 21:12:47 +0100

    Implement search feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import {robots} from "./robots";
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        };
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value});
+    };
+
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
+
         return (
             <div className="container">
-                <CardList />
+                <div className="section">
+                    <div className="columns">
+                        <div className="column is-two-fifths">
+                            <SearchBox searchChange={this.onSearchChange}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="section">
+                    <CardList robots={filteredRobots}/>
+                </div>
             </div>
         );
     }

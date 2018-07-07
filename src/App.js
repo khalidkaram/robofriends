560140bb4e2f479bef1071ba126bb510dd671944
Khalid <kar.khalid@gmail.com>
2018-07-07 17:02:06 +0100

    Move App component to separate file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardList from "./CardList";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <CardList />
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CardList from './CardList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <CardList />
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

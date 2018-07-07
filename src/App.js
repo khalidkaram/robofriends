525d428eb60dd667e769cd1f998de37002c4c18b
Khalid <kar.khalid@gmail.com>
2018-07-07 23:22:31 +0100

    Fetch data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {robots} from "./robots";
+import axios from "axios";
 
 export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
     }
@@ -15,6 +15,13 @@ export default class App extends React.Component {
     onSearchChange = (event) => {
         this.setState({searchField: event.target.value});
     };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                this.setState({robots: response.data});
+            });
+    }
 
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
